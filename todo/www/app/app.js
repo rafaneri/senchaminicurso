3532f724c43e01478b0a6b5271dbda8c806520b3
Rafael Neri <rafael.neri@gmail.com>
2015-11-10 03:03:43 -0300

    - Ajustando textos sim e não

--- todo/www/app/app.js
+++ todo/www/app/app.js
@@ -1,5 +1,14 @@
-
-
+/**
+* Define a aplicação sencha touch
+*/
+var MB = Ext.MessageBox;
+Ext.apply(MB, {
+        YES: { text: 'Sim', itemId: 'yes', ui: 'action' },
+        NO: { text: 'Não', itemId: 'no' }
+});
+Ext.apply(MB, {
+        YESNO: [MB.NO, MB.YES]
+});
 Ext.Loader.setConfig({
     paths: {
         Ext: '../js'
