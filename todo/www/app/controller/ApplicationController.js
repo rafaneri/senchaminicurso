320bd5ccbdde9a67711e27e8e1440d886ba93497
Rafael Neri <rafael.neri@gmail.com>
2015-11-10 01:10:21 -0300

    - Mantendo o titulo das tabs na navgationbar

--- todo/www/app/controller/ApplicationController.js
+++ todo/www/app/controller/ApplicationController.js
@@ -4,14 +4,20 @@
 Ext.define('Notas.controller.ApplicationController', {
     extend: 'Ext.app.Controller',
     config: { 
+        // mantém o titulo da view antes de sair para o form
+        currentTitle: '',
         refs: {
             main: 'mainview',
+            tabnavigation: 'maintabnavigation',
             btnAdd: '#btnAdd',
             grupoform: 'grupoform',
             grupolist: 'grupolist',
         },
 
         control: {
+            tabnavigation:{
+                tabItemChange: 'onTabItemChange'
+            },
             btnAdd: {
                 tap: 'onAdd'
             },
@@ -23,6 +29,21 @@ Ext.define('Notas.controller.ApplicationController', {
                 exibirGrupo: 'onExibirGrupo'
             },
         }
+    },
+    
+    onTabItemChange: function(el, value, oldValue, eOpts) {
+        var text = value.tab.getTitle();
+        this.setMainTitle(text);
+    },
+    
+    getMainTitle: function() {
+        var mainTitleBar = this.getMain().getNavigationBar();
+        return mainTitleBar.getTitle();
+    },
+
+    setMainTitle: function(title) {
+        var mainTitleBar = this.getMain().getNavigationBar();
+        mainTitleBar.setTitle(title);
     },
 
     onAdd: function(el, e, eOpts) {
@@ -39,17 +60,20 @@ Ext.define('Notas.controller.ApplicationController', {
         var store = Ext.data.StoreManager.lookup('GrupoStore');
         store.add(record);
         this.getMain().pop();
+        this.setMainTitle(this.getCurrentTitle());
     },
     
     onRemoverGrupo: function(record) {
         var store = Ext.data.StoreManager.lookup('GrupoStore');
         store.remove(record);
         this.getMain().pop();
+        this.setMainTitle(this.getCurrentTitle());
     },
     
     onExibirGrupo: function(list, index, node, record) {
+        this.setCurrentTitle(this.getMainTitle());
         if (!this.grupoform) {
-            this.grupoform = Ext.create('Notas.view.GrupoForm');
+            this.grupoform = Ext.create('NotasErbase.view.GrupoForm');
         }
         this.grupoform.setRecord(record, true);
         this.grupoform.setTitle('Editar Grupo');
