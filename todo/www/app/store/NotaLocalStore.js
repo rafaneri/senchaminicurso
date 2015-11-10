3e1f170f65bb3e075987b6cc93008fc0824bb729
Rafael Neri <rafael.neri@gmail.com>
2015-11-10 00:04:53 -0300

    - Criando o data store para as notas locais

---
+++ todo/www/app/store/NotaLocalStore.js
@@ -0,0 +1,21 @@
+/**
+* Define o data store para o objeto Nota
+*/
+Ext.define('Notas.store.NotaLocalStore', {
+    extend: 'Ext.data.Store',
+    config: {
+        model: 'Notas.model.Nota',
+        sorters: [{property: '_id', direction: 'DESC'}],
+        grouper: {
+            groupFn: function(record) {
+                return record.getGrupo().get('nome');
+            },
+            sortProperty: 'grupo_id'
+        },
+        autoLoad: true,
+        proxy: {
+            type: 'localstorage',
+            id  : 'notas-locais'
+        }
+    }
+});
